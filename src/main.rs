06b0b453accd337385eabea33d3a961e0b2e9014
freek <flijten@proton.me>
2025-12-23 11:56:07 +0100

    Day 5

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::fs;
 
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone)]
 struct Range { start: i64, end: i64}
 
 impl Range {
@@ -10,6 +10,11 @@ impl Range {
 }
 
 fn main() {
+    assignment1();
+    assignment2();
+}
+
+fn assignment1() {
     let contents = fs::read_to_string("src/day5")
         .expect("Failed to read file");
     let mut ranges: Vec<Range> = Vec::new();
@@ -19,7 +24,6 @@ fn main() {
     let mut number_of_fresh_ingredients: i64 = 0;
 
     for line in contents.lines() {
-
         if line.is_empty() {
             finding_ranges = false; //switch to checking ingredients
             continue;
@@ -30,7 +34,7 @@ fn main() {
             let start: i64 = start_str.parse().unwrap();
             let end: i64 = end_str.parse().unwrap();
 
-            ranges.push(Range { start: start, end: end})
+            ranges.push(Range { start: start, end: end })
         } else {
             let ingredient_id: i64 = line.parse().unwrap();
             let is_fresh = ranges.iter().any(|range| range.contains(ingredient_id));
@@ -40,6 +44,45 @@ fn main() {
         }
     }
 
+    println!("assignment 1");
     println!("{:#?}", ranges);
     println!("{}", number_of_fresh_ingredients);
+}
+
+fn assignment2() {
+    let contents = fs::read_to_string("src/day5")
+        .expect("Failed to read file");
+    let mut ranges: Vec<Range> = Vec::new();
+    let mut merged: Vec<Range> = Vec::new();
+
+    for line in contents.lines() {
+        if line.is_empty() {
+            break;
+        }
+        let (start_str, end_str) = line.split_once('-').unwrap();
+        let start: i64 = start_str.parse().unwrap();
+        let end: i64 = end_str.parse().unwrap();
+
+        ranges.push(Range { start: start, end: end })
+    }
+
+    ranges.sort_by_key(|range| range.start); //sort ranges on start
+    merged.push(ranges[0]); // Start with first range
+
+    for (range) in ranges.into_iter().skip(1) {
+        let last_range_in_set = merged.last_mut().unwrap();
+
+        if range.start <= last_range_in_set.end + 1 {
+            last_range_in_set.end = last_range_in_set.end.max(range.end);  // Merge into last range
+        } else {
+            merged.push(range);  // Add as new range
+        }
+    }
+
+    println!("assignment 2");
+    let total_count: i64 = merged.iter()
+        .map(|range| range.end - range.start + 1)
+        .sum();
+
+    println!("Total numbers covered: {}", total_count);
 }
